520ef89db666c07a6585d251fb50beb8ee25367c
flippybit <flippybit@noemail.com>
2022-11-14 23:06:13 +0100

    added sidebar

--- docs/conf.py
+++ docs/conf.py
@@ -2,6 +2,10 @@ html_theme = "pydata_sphinx_theme"
 master_doc = 'index'
 
 extensions = ['myst_parser']
+html_sidebars = {
+    "**": ["sidebar-nav-bs", "sidebar-ethical-ads"]
+}
+
 html_theme_options = {
   "show_nav_level": 2
 }
