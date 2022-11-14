c255c565e74feef1c2986a9a0f4a11113091cb62
flippybit <flippybit@noemail.com>
2022-11-14 23:16:34 +0100

    more changes to the navbar

--- docs/conf.py
+++ docs/conf.py
@@ -2,9 +2,6 @@ html_theme = "pydata_sphinx_theme"
 master_doc = 'index'
 
 extensions = ['myst_parser']
-html_sidebars = {
-    "**": ["sidebar-nav-bs", "sidebar-ethical-ads"]
-}
 
 
 html_theme_options = {
