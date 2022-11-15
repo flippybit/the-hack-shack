b4c1e9117dde1cbeef91f3c0dc54476bc0aefc26
flippybit <flippybit@noemail.com>
2022-11-15 01:45:35 +0100

    installed furo as a theme

--- docs/conf.py
+++ docs/conf.py
@@ -1,10 +1,4 @@
-html_theme = "pydata_sphinx_theme"
+html_theme = "furo"
 master_doc = 'index'
 html_logo = "imgs/shack.jpeg"
 extensions = ['myst_parser']
-html_sidebars = {
-    "**": ["sidebar-nav-bs","sidebar-nav-bs.html" "sidebar-ethical-ads"]
-}
-html_theme_options = {
-  "show_nav_level": 2
-}
