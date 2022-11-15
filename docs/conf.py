186e9d861520f21d125ce5d162fa1bea1c83529d
flippybit <flippybit@noemail.com>
2022-11-15 02:20:25 +0100

    changed back to FURO

--- docs/conf.py
+++ docs/conf.py
@@ -1,9 +1,10 @@
-html_theme = "pydata-sphinx-theme"
+html_theme = "furo"
 html_title = "the-hack-shack"
 master_doc = 'index'
 html_logo = "imgs/shack.jpeg"
 extensions = [
   "myst_parser",
+  "sphinx_copybutton",
   "sphinx_design",
   "sphinx_inline_tabs",
   ]
