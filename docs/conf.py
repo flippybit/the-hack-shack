99653d686f2c9c1b56268cee06185d33dd413729
flippybit <flippybit@noemail.com>
2022-11-15 02:35:26 +0100

    deletec all extensions

--- docs/conf.py
+++ docs/conf.py
@@ -3,8 +3,5 @@ html_title = "the-hack-shack"
 master_doc = 'index'
 html_logo = "imgs/shack.jpeg"
 extensions = [
-  "myst_parser",
-  "sphinx_copybutton",
-  "sphinx_design",
-  "sphinx_inline_tabs",
+  "myst_parser"
   ]
