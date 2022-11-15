4823fe1a66641ee965cd64c6dd223e998b47fa04
flippybit <flippybit@noemail.com>
2022-11-15 02:09:31 +0100

    fixed comma on the conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -3,7 +3,7 @@ html_title = "the-hack-shack"
 master_doc = 'index'
 html_logo = "imgs/shack.jpeg"
 extensions = [
-  "myst_parser"
+  "myst_parser",
   "sphinx_copybutton",
   "sphinx_design",
   "sphinx_inline_tabs",
