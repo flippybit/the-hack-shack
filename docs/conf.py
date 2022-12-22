879f29f8229de8b8dec3fcc3d96dc1a7a7816a6c
stocker <stocker@laptop.com>
2022-12-22 07:13:57 +0100

    this is another

--- docs/conf.py
+++ docs/conf.py
@@ -2,17 +2,7 @@ html_theme = "furo"
 html_title = "the-hack-shack"
 master_doc = 'index'
 html_logo = "imgs/shack.jpeg"
-
 extensions = [
-    # Sphinx's own extensions
-    "sphinx.ext.autodoc",
-    "sphinx.ext.extlinks",
-    "sphinx.ext.intersphinx",
-    "sphinx.ext.mathjax",
-    "sphinx.ext.todo",
-    "sphinx.ext.viewcode",
-    # External stuff
-    "myst_parser",
-    "sphinx_design",
-    "sphinx_inline_tabs",
-]
+  "myst_parser"
+  ]
+
